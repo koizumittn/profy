30ee52b114b9e5d3dab7ac92003af475fdf8a074
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-23 14:41:14 +0900

    認証を行うキーにgroup_keyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, authentication_keys: [:email, :group_key]
   # accesor
   belongs_to :group
   attr_accessor 'group_key'
