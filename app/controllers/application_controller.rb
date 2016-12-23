b20a2120dde548b45d889aa3c2bf823f9748d77f
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-23 16:27:28 +0900

    ストロングパラメータの追加を1行にまとめる。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    # sign_inのときに、group_keyも許可する
-      devise_parameter_sanitizer.permit(:sign_in, keys:[:group_key])
-    # sign_upのときに、group_keyも許可する
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:group_key])
-    #account_updateのときに、group_keyも許可する
-      devise_parameter_sanitizer.permit(:account_update, keys:[:group_key])
+    # # # sign_inのときに、group_keyも許可する
+    #   devise_parameter_sanitizer.permit(:sign_in, keys:[:group_key])
+    # # sign_upのときに、group_keyも許可する
+    #   devise_parameter_sanitizer.permit(:sign_up, keys:[:group_key])
+    # # account_updateのときに、group_keyも許可する
+    #   devise_parameter_sanitizer.permit(:account_update, keys:[:group_key])
+
+    # ストロングパラメータの設定を1つにまとめられるか？
+    # →OK!
+      devise_parameter_sanitizer.permit([:sign_in, :sign_up, :account_update], keys:[:group_key])
 
   end
 end
