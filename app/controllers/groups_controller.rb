2dc50dd3e53e875f9d4a59afa76c691022b0de68
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-25 18:36:52 +0900

    グループの編集機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,9 +5,17 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    @group.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:group).permit(:key, :detail)
   end
 
 end
