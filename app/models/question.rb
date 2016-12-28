f57c8c24987da0c0d84af43fd85150d8dd1ec4d6
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-28 17:32:43 +0900

    ユーザページで回答済み質問を閲覧できるようにする。

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,4 +9,8 @@ class Question < ActiveRecord::Base
   # validation
   validates_presence_of :text, :user_id, :group_id
 
+  def user_answer(user_id)
+    Answer.find_by(user_id: user_id, question_id: id)
+  end
+
 end
