64c9db150c1a3ce4e072ca626c14f6d95bbdc1db
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-28 17:57:30 +0900

    回答の編集機能を実装

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,9 +12,25 @@ class AnswersController < ApplicationController
     Answer.create(create_params)
   end
 
+  def edit
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.update(update_params)
+  end
+
+
   private
   def create_params
     params.require(:answer).permit(:text, :question_id).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:answer).permit(:text)
   end
 
   def redirect
