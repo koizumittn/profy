e220404bc3491672ac18ab4c2360aa08bdf2e7f4
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-30 20:28:43 +0900

    メソッド名の誤りを修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     @question = Question.new
-    feed_contents = current_user.gourp.feed_contents.includes(:content)
+    feed_contents = current_user.group.feed_contents.includes(:content)
     @feed_contents = feed_contents.map(&:content)
   end
 
