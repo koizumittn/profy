0b51df8e8a2fef65cfb3a7e2497decc1c9666806
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-30 17:46:35 +0900

    ポリモーフィック関連の定義。グループIDと更新日時を同期してfeed_contensテーブルに記録するようにコールバックメソッド(after_create)を定義

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,17 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
 
+  has_one :feed_content, as: :content, dependent: :destroy
+
+  #callback
+  after_create :create_feed_content
+
+  #validation
+  validates_presence_of :user_id, :text
+
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: question.group_id, updated_at: updated_at)
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,13 @@
 class Question < ActiveRecord::Base
 
+  #callback
+  after_create :create_feed_content
+
   # association
   belongs_to :user
   belongs_to :question
   has_many :answers
+  has_one :feed_content, as: :content, dependent: :destroy
 
 
   # validation
@@ -11,6 +15,11 @@ class Question < ActiveRecord::Base
 
   def user_answer(user_id)
     Answer.find_by(user_id: user_id, question_id: id)
+  end
+
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: group_id, updated_at: updated_at)
   end
 
 end
